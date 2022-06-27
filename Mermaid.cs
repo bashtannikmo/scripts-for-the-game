49b674cb37ac9599387fb13bc928fd6c7fdf2293
bashtannikmo <108298985+bashtannikmo@users.noreply.github.com>
2022-06-27 22:36:25 +0300

    Add files via upload

---
+++ Mermaid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mermaid : Entity
+{
+    [SerializeField] private int lives = 3;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == Hero.Instance.gameObject)
+        {
+            Hero.Instance.GetDamage();
+            lives--;
+            Debug.Log("У русалки: " + lives);
+        
+        }
+        if (lives < 1)
+        Die();
+    }
+}
